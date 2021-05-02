c4aafff9bdd260035ce91c8afde3f33f057c92ea
Fatima <fatima.hammouri@gmail.com>
2021-05-02 10:16:16 +0000

    Created Cuisine Model

--- model.py
+++ model.py
@@ -5,9 +5,24 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
+####################################################################################
+
+class Cuisine(db.Model):
+"""the countries table"""
+
+    __tablename__ = 'cuisines'
+
+    cuisine_id = db.Column(db.Integer, primary_key=True,
+    autoincrement=True)
+    cuisine_name = db.Column(db.String(50))
 
 
-def connect_to_db(flask_app, db_uri='postgresql:///recipes', echo=True):
+    def __repr__(self):
+    return f'<Country country_code={self.country_code} contry_name={self.contry_name}>’
+
+####################################################################################
+
+def connect_to_db(flask_app, db_uri='postgresql:///recipe', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
