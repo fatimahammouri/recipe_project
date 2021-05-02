ad8385c530d7dbcd2f5e50c19e8cd9d8764b94d8
Fatima <fatima.hammouri@gmail.com>
2021-05-02 10:14:16 +0000

    initial set up

--- static/hello.jsx
+++ static/hello.jsx
@@ -1,15 +1,2 @@
 'use strict';
 console.log('hello fatima');
-
-function Hello(props) {
-
-    // with jsx and Babel
-    return (<h1> Hello from react with jsx</h1>);
-
-    // without Babel and jsx
-    // return React.createElement('h1', null, 'Hello world (from React)');
-}
-ReactDOM.render(
-    React.createElement(Hello), //here we can do <Hello />,
-    document.getElementById('reactGo'),
-);
