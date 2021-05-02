10eea2b49c1464f993b6aa7c2657573a314b6f32
Fatima <fatima.hammouri@gmail.com>
2021-05-02 10:40:55 +0000

    added homepage route

--- server.py
+++ server.py
@@ -3,6 +3,15 @@ from jinja2 import StrictUndefined
 from model import connect_to_db
 import crud
 
+##########################################################################
+
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html’)
+
+
+##########################################################################
 
 connect_to_db(app, echo=False)
 app.run(host='0.0.0.0', debug=True, port=5000)
