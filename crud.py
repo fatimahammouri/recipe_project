756c5935913bfeab9553783387e01e8d5fef9055
Fatima <fatima.hammouri@gmail.com>
2021-05-02 10:24:38 +0000

    declared function to create &  add cuisine to db

--- crud.py
+++ crud.py
@@ -1,2 +1,12 @@
 from model import (db, User, Recipe, Cuisine, Ingredient,
                 Ingredient_recipe, recipe_cuisine, connect_to_db)
+
+
+
+def create_cuisine(cuisine_name):
+
+    cuisine = Cuisine(cuisine_name=cuisine_name) 
+    db.session.add(cuisine)
+    db.session.commit()
+
+    return cuisine
