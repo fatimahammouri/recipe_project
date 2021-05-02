1848bbb5e0b8d61ff4e591675e69bd7c5b5c319c
Fatima <fatima.hammouri@gmail.com>
2021-05-02 10:40:19 +0000

    created basic homepage

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from jinja2 import StrictUndefined
 from model import connect_to_db
 import crud
 
-if __name__ == '__main__':
 
-    connect_to_db(app, echo=False)
-    app.run(host='0.0.0.0', debug=True, port=5000)
+connect_to_db(app, echo=False)
+app.run(host='0.0.0.0', debug=True, port=5000)
