2cef429993e4cdf5791fe2f759c125fe77530df8
Fatima <fatima.hammouri@gmail.com>
2021-05-02 10:33:46 +0000

    connect to db & run on port

--- server.py
+++ server.py
@@ -2,3 +2,8 @@ from flask import Flask, render_template, request, flash, session, redirect
 from jinja2 import StrictUndefined
 from model import connect_to_db
 import crud
+
+if __name__ == '__main__':
+
+    connect_to_db(app, echo=False)
+    app.run(host='0.0.0.0', debug=True, port=5000)
