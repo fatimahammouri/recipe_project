89127487324eb2cfb12059f2967a7860e4032e8d
Fatima <fatima.hammouri@gmail.com>
2021-05-03 11:33:58 +0000

    uniting js style

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -6,12 +6,12 @@ let listCuisineItems = document.querySelectorAll(".list_cuisine_item");
 
 listCuisineItems.forEach((item, index)=>{
 
-    item.addEventListener('click', (event)=>{
+    item.addEventListener("click", (event)=>{
         let selectedCuisine = event.currentTarget.innerHTML;
         console.log(selectedCuisine)
-
+    
         const recipeCards = document.querySelector("#recipe_cards")
-        $.post('/results/'+ selectedCuisine ,(response)=>{
+        $.post("/results/"+ selectedCuisine ,(response)=>{
             recipeCards.innerHTML = response;   
         }); 
     });
