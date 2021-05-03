9ba4d40368f365bc9ada7afc3e1bd470e0c3c234
Fatima <fatima.hammouri@gmail.com>
2021-05-03 11:17:43 +0000

    create a recipeCards variable to hold the queried element

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -3,10 +3,14 @@
 
 
 let listCuisineItems = document.querySelectorAll(".list_cuisine_item");
+
 listCuisineItems.forEach((item, index)=>{
+
     item.addEventListener('click', (event)=>{
         let selectedCuisine = event.currentTarget.innerHTML;
         console.log(selectedCuisine)
+        
+        const recipeCards = document.querySelector("#recipe_cards")
         $.post('/results/'+ selectedCuisine ,(response)=>{
             $('#recipe_cards').html(response)
         }); 
