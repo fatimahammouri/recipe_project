f96b61fbf955ae41e859ac0af056ab69972f092b
Fatima <fatima.hammouri@gmail.com>
2021-05-17 22:39:08 +0000

    added crud function to save recipe created by user to db

--- crud.py
+++ crud.py
@@ -19,3 +19,20 @@ def get_all_cuisines():
     for cuisine in Cuisine.query.all():
         cuisine_list.append(cuisine.cuisine_name)
     return cuisine_list
+
+##########################################################################
+
+def create_recipe(title, image, servings, ready_in_minutes, instructions, ingredients, cuisine_name):
+    recipe = Recipe(title=title, 
+                    image=image, 
+                    servings=servings, 
+                    ready_in_minutes=ready_in_minutes,
+                    instructions=instructions,
+                    ingredients=ingredients, 
+                    cuisine_name=cuisine_name)
+    db.session.add(recipe)
+    db.session.commit()
+
+    return recipe        
+
+    
