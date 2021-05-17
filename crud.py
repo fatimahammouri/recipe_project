f30e5d861d8a369f54469d8eb33099646ceb7abd
Fatima <fatima.hammouri@gmail.com>
2021-05-17 22:16:26 +0000

    fixed crud function thet querys all cuisine name from db

--- crud.py
+++ crud.py
@@ -13,4 +13,9 @@ def create_cuisine(cuisine_name):
 ##########################################################################
 
 def get_all_cuisines():
-    return Cuisine.query.all()
+
+    cuisine_list = []
+
+    for cuisine in Cuisine.query.all():
+        cuisine_list.append(cuisine.cuisine_name)
+    return cuisine_list
